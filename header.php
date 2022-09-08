0ae1303a20b02f41906df2c4865328acd88e40ca
Hein Soe <hi@heinsoe.com>
2022-09-08 21:22:48 +0630

    apply_filters('the_content', ->post_content)

--- header.php
+++ header.php
@@ -3,7 +3,14 @@ header('Content-Type: application/json; charset=utf-8');
 // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 header("Access-Control-Allow-Origin: * ");
 header('Access-Control-Allow-Credentials: false');
-header('Access-Control-Allow-Method: GET,POST,OPTIONS');
+header('Access-Control-Allow-Method: GET');
 header('Access-Control-Max-Age: 86400');
 
-?>
+
+
+// header("Cache-Control: public, must-revalidate"); //HTTP 1.1
+// header("Pragma: public"); //HTTP 1.0
+// // header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+// //or, if you DO want a file to cache, use:
+// header("Cache-Control: max-age=2 592000");
--- single.php
+++ single.php
@@ -6,7 +6,7 @@ echo json_encode(array(
 	"slug" => $post->post_name,
     "time" => date("Y-m-d H:i:s", get_post_time('U', true)),
     "title"=>get_the_title(),
-    "content"=>apply_filters('the_content',get_the_content()),
+    "content"=>apply_filters('the_content', $post->post_content),
     "excerpt" => html_entity_decode(get_the_excerpt(), ENT_QUOTES, 'UTF-8'),
     "images" => images(),
     "category" => wp_get_post_terms(get_the_ID(), "category"),
